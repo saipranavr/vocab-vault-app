897558df41f7084fa2490aaecd80e79af63db9c7
pranav reddy <saipranav919@gmail.com>
2024-02-04 23:13:18 +0530

    working inital switch to google dictionary third party api

--- lib/network/owlbot_get_api.dart
+++ lib/network/owlbot_get_api.dart
@@ -3,11 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 class OwlBotGetAPI {
-  final String _url = "https://owlbot.info/api/v4/dictionary/";
-  final String _token = "e014ed619e6f5dd7b7544a9457fc7d322c9992d3";
+  final String _url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
-  Future<Map<String, dynamic>> searchWord(String word) async {
-    Response response = await get(Uri.parse(_url + word.trim()), headers: {"Authorization": "Token $_token"});
+  Future<List<dynamic>> searchWord(String word) async {
+    Response response = await get(Uri.parse(_url + word.trim()));
     return json.decode(response.body);
   }
 }
